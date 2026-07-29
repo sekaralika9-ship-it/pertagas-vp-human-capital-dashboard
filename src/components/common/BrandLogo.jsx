c0872e426e08dd53603a5be8a296157a4a71fdc3
alika sekar <sekaralika9@gmail.com>
2026-07-29 11:11:00 +0700

    Replace branding with Pertamina Gas Negara logo

--- src/components/common/BrandLogo.jsx
+++ src/components/common/BrandLogo.jsx
@@ -2,9 +2,8 @@ import logo from '../../assets/company-logo.png';
 
 export default function BrandLogo({ compact = false }) {
   return (
-    <div className="flex items-center gap-3">
-      {/* Use src/assets/pertagas-logo.png here if the organisation supplies a newer official master asset. */}
-      <img src={logo} alt="Pertamina Gas" className="h-11 w-auto max-w-[155px] object-contain" />
+    <div className="inline-flex items-center rounded-xl bg-white px-3 py-2">
+      <img src={logo} alt="Pertamina Gas Negara" className="h-11 w-auto max-w-[185px] object-contain" />
       {!compact && <span className="sr-only">HC Operation Dashboard</span>}
     </div>
   );
