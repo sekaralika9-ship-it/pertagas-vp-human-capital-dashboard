88bb1787444329b3c38335340052a7463571b667
alika sekar <sekaralika9@gmail.com>
2026-07-29 11:17:46 +0700

    Correct logo to Pertamina Gas mark

--- src/components/common/BrandLogo.jsx
+++ src/components/common/BrandLogo.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/company-logo.png';
 export default function BrandLogo({ compact = false }) {
   return (
     <div className="inline-flex items-center rounded-xl bg-white px-3 py-2">
-      <img src={logo} alt="Pertamina Gas Negara" className="h-11 w-auto max-w-[185px] object-contain" />
+      <img src={logo} alt="Pertamina Gas" className="h-11 w-auto max-w-[185px] object-contain" />
       {!compact && <span className="sr-only">HC Operation Dashboard</span>}
     </div>
   );
